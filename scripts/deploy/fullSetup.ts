024efe30a2937e06721f2d5f4fccf0ec56daa56b
web3sdkio <demo@web3sdks.com>
2023-01-04 17:08:33 +0800

    Init scripts

--- scripts/deploy/fullSetup.ts
+++ scripts/deploy/fullSetup.ts
@@ -28,9 +28,9 @@ var deployContracts = {
     "className": "Forwarder",
     "deployedAddresses": {
       "1": "", // mainnet
-      "5": "0x5D1021751de591fEd8562eE92731F6D612D4cC81", // goerli
+      "5": "", // goerli
       "137": "", // polygon
-      "80001": "0x83b69703543B7240b2e8218f9Ca21255e007AD49", // mumbai
+      "80001": "", // mumbai
       "250": "", // fantom
       "4002": "", // fantom_testnet
       "43114": "", // avax
@@ -47,9 +47,9 @@ var deployContracts = {
     "className": "TWRegistry",
     "deployedAddresses": {
       "1": "", // mainnet
-      "5": "0xB161f6EA00A3008F20a32d465E5364B0905A33Ad", // goerli
+      "5": "", // goerli
       "137": "", // polygon
-      "80001": "0x535Dc6e14C23623145e396271cc45858CCE0F21c", // mumbai
+      "80001": "", // mumbai
       "250": "", // fantom
       "4002": "", // fantom_testnet
       "43114": "", // avax
@@ -69,9 +69,9 @@ var deployContracts = {
     "className": "TWFactory",
     "deployedAddresses": {
       "1": "", // mainnet
-      "5": "0x80a1d6255191D2Af220a35640F393de5B6266f02", // goerli
+      "5": "", // goerli
       "137": "", // polygon
-      "80001": "0xA3033c3d137b58edC315362c5B1858613b88c61C", // mumbai
+      "80001": "", // mumbai
       "250": "", // fantom
       "4002": "", // fantom_testnet
       "43114": "", // avax
@@ -94,9 +94,9 @@ var deployContracts = {
     "className": "TWFee",
     "deployedAddresses": {
       "1": "", // mainnet
-      "5": "0x2d484d217F3CF9C0AF4E4bE27825B946B0F1049F", // goerli
+      "5": "", // goerli
       "137": "", // polygon
-      "80001": "0xc18DE50c8513FE9F5d0e30FE25CC9082229C95F0", // mumbai
+      "80001": "", // mumbai
       "250": "", // fantom
       "4002": "", // fantom_testnet
       "43114": "", // avax
@@ -116,16 +116,16 @@ var deployContracts = {
     },
     "needPack": true,
     "needVerify": true,
-    "packed": true,
-    "verified": true,
+    "packed": false,
+    "verified": false,
   },
   "DropERC721": {
     "className": "DropERC721",
     "deployedAddresses": {
       "1": "", // mainnet
-      "5": "0x20e099109dbEbBD11058EeB39417B2eCA52C83f0", // goerli
+      "5": "", // goerli
       "137": "", // polygon
-      "80001": "0xc2945698dc0b20bE08d0344F27F41eA26655134b", // mumbai
+      "80001": "", // mumbai
       "250": "", // fantom
       "4002": "", // fantom_testnet
       "43114": "", // avax
@@ -139,16 +139,16 @@ var deployContracts = {
     },
     "needPack": true,
     "needVerify": true,
-    "packed": true,
-    "verified": true,
+    "packed": false,
+    "verified": false,
   },
   "DropERC1155": {
     "className": "DropERC1155",
     "deployedAddresses": {
       "1": "", // mainnet
-      "5": "0xEE89E8E54271F30738883e28DD7f8226805a6848", // goerli
+      "5": "", // goerli
       "137": "", // polygon
-      "80001": "0x1274317550F24C45d1F035Ee53D89df02B0AeA84", // mumbai
+      "80001": "", // mumbai
       "250": "", // fantom
       "4002": "", // fantom_testnet
       "43114": "", // avax
@@ -162,16 +162,16 @@ var deployContracts = {
     },
     "needPack": true,
     "needVerify": true,
-    "packed": true,
-    "verified": true,
+    "packed": false,
+    "verified": false,
   },
   "DropERC20": {
     "className": "DropERC20",
     "deployedAddresses": {
       "1": "", // mainnet
-      "5": "0x5Abe68be6ecE6586Bd438BE25cEed2D5707F2a95", // goerli
+      "5": "", // goerli
       "137": "", // polygon
-      "80001": "0x2ea37Ba764e631F9C0883F1994D221a5398FC7b3", // mumbai
+      "80001": "", // mumbai
       "250": "", // fantom
       "4002": "", // fantom_testnet
       "43114": "", // avax
@@ -185,16 +185,16 @@ var deployContracts = {
     },
     "needPack": true,
     "needVerify": true,
-    "packed": true,
-    "verified": true,
+    "packed": false,
+    "verified": false,
   },
   "TokenERC20": {
     "className": "TokenERC20",
     "deployedAddresses": {
       "1": "", // mainnet
-      "5": "0x07Fd3DAdf1f81BfA0ab0841c33e44789E78335F0", // goerli
+      "5": "", // goerli
       "137": "", // polygon
-      "80001": "0x212232D6500E9b3CFD9CaEED512bF179622F8Ca6", // mumbai
+      "80001": "", // mumbai
       "250": "", // fantom
       "4002": "", // fantom_testnet
       "43114": "", // avax
@@ -208,16 +208,16 @@ var deployContracts = {
     },
     "needPack": true,
     "needVerify": true,
-    "packed": true,
-    "verified": true,
+    "packed": false,
+    "verified": false,
   },
   "TokenERC721": {
     "className": "TokenERC721",
     "deployedAddresses": {
       "1": "", // mainnet
-      "5": "0x2F7f4Ac01119F8F99B94F0927EfA27D7818A074b", // goerli
+      "5": "", // goerli
       "137": "", // polygon
-      "80001": "0x5D1021751de591fEd8562eE92731F6D612D4cC81", // mumbai
+      "80001": "", // mumbai
       "250": "", // fantom
       "4002": "", // fantom_testnet
       "43114": "", // avax
@@ -231,16 +231,16 @@ var deployContracts = {
     },
     "needPack": true,
     "needVerify": true,
-    "packed": true,
-    "verified": true,
+    "packed": false,
+    "verified": false,
   },
   "TokenERC1155": {
     "className": "TokenERC1155",
     "deployedAddresses": {
       "1": "", // mainnet
-      "5": "0x436bC195D4BFFD9aA0DfB20688737cF7D7Ab3e35", // goerli
+      "5": "", // goerli
       "137": "", // polygon
-      "80001": "0xB161f6EA00A3008F20a32d465E5364B0905A33Ad", // mumbai
+      "80001": "", // mumbai
       "250": "", // fantom
       "4002": "", // fantom_testnet
       "43114": "", // avax
@@ -254,16 +254,16 @@ var deployContracts = {
     },
     "needPack": true,
     "needVerify": true,
-    "packed": true,
-    "verified": true,
+    "packed": false,
+    "verified": false,
   },
   "Split": {
     "className": "Split",
     "deployedAddresses": {
       "1": "", // mainnet
-      "5": "0xC5a73A3794423CB30029ab5c6d0acb22f6CcCDA2", // goerli
+      "5": "", // goerli
       "137": "", // polygon
-      "80001": "0x80a1d6255191D2Af220a35640F393de5B6266f02", // mumbai
+      "80001": "", // mumbai
       "250": "", // fantom
       "4002": "", // fantom_testnet
       "43114": "", // avax
@@ -277,16 +277,16 @@ var deployContracts = {
     },
     "needPack": true,
     "needVerify": true,
-    "packed": true,
-    "verified": true,
+    "packed": false,
+    "verified": false,
   },
   "Marketplace": {
     "className": "Marketplace",
     "deployedAddresses": {
       "1": "", // mainnet
-      "5": "0x28F97d65F66F5D4f152e9A46Cf3D6DF22307e86d", // goerli
+      "5": "", // goerli
       "137": "", // polygon
-      "80001": "0x2d484d217F3CF9C0AF4E4bE27825B946B0F1049F", // mumbai
+      "80001": "", // mumbai
       "250": "", // fantom
       "4002": "", // fantom_testnet
       "43114": "", // avax
@@ -305,16 +305,16 @@ var deployContracts = {
     },
     "needPack": true,
     "needVerify": true,
-    "packed": true,
-    "verified": true,
+    "packed": false,
+    "verified": false,
   },
   "VoteERC20": {
     "className": "VoteERC20",
     "deployedAddresses": {
       "1": "", // mainnet
-      "5": "0x73ab01d8Eeb0Ac0C012BC1ca20d5A0494C38a376", // goerli
+      "5": "", // goerli
       "137": "", // polygon
-      "80001": "0xfCbC4b66C6fa60cDB1636dCDCE02c6137d5Ebb31", // mumbai
+      "80001": "", // mumbai
       "250": "", // fantom
       "4002": "", // fantom_testnet
       "43114": "", // avax
@@ -328,16 +328,16 @@ var deployContracts = {
     },
     "needPack": true,
     "needVerify": true,
-    "packed": true,
-    "verified": true,
+    "packed": false,
+    "verified": false,
   },
   "Multiwrap": {
     "className": "Multiwrap",
     "deployedAddresses": {
       "1": "", // mainnet
-      "5": "0x831e2B04c4aF9a3759C5D8FeA49692e8a0294B01", // goerli
+      "5": "", // goerli
       "137": "", // polygon
-      "80001": "0xf9a496Ac51b578DBE2228475D5b49636881C3B53", // mumbai
+      "80001": "", // mumbai
       "250": "", // fantom
       "4002": "", // fantom_testnet
       "43114": "", // avax
@@ -356,16 +356,16 @@ var deployContracts = {
     },
     "needPack": true,
     "needVerify": true,
-    "packed": true,
-    "verified": true,
+    "packed": false,
+    "verified": false,
   },
   "SignatureDrop": {
     "className": "SignatureDrop",
     "deployedAddresses": {
       "1": "", // mainnet
-      "5": "0xFDb9cc34BfA6a51e96F73f0a828717A6dfA460E8", // goerli
+      "5": "", // goerli
       "137": "", // polygon
-      "80001": "0xa6E25788347CD71A319Fcac6Dd74cC476843cBe8", // mumbai
+      "80001": "", // mumbai
       "250": "", // fantom
       "4002": "", // fantom_testnet
       "43114": "", // avax
@@ -379,16 +379,16 @@ var deployContracts = {
     },
     "needPack": true,
     "needVerify": true,
-    "packed": true,
-    "verified": true,
+    "packed": false,
+    "verified": false,
   },
   "AirdropERC20": {
     "className": "AirdropERC20",
     "deployedAddresses": {
       "1": "", // mainnet
-      "5": "0xFDb9cc34BfA6a51e96F73f0a828717A6dfA460E8", // goerli
+      "5": "", // goerli
       "137": "", // polygon
-      "80001": "0x73ab01d8Eeb0Ac0C012BC1ca20d5A0494C38a376", // mumbai
+      "80001": "", // mumbai
       "250": "", // fantom
       "4002": "", // fantom_testnet
       "43114": "", // avax
@@ -402,16 +402,16 @@ var deployContracts = {
     },
     "needPack": true,
     "needVerify": true,
-    "packed": true,
-    "verified": true,
+    "packed": false,
+    "verified": false,
   },
   "AirdropERC721": {
     "className": "AirdropERC721",
     "deployedAddresses": {
       "1": "", // mainnet
-      "5": "0xFDb9cc34BfA6a51e96F73f0a828717A6dfA460E8", // goerli
+      "5": "", // goerli
       "137": "", // polygon
-      "80001": "0x831e2B04c4aF9a3759C5D8FeA49692e8a0294B01", // mumbai
+      "80001": "", // mumbai
       "250": "", // fantom
       "4002": "", // fantom_testnet
       "43114": "", // avax
@@ -425,16 +425,16 @@ var deployContracts = {
     },
     "needPack": true,
     "needVerify": true,
-    "packed": true,
-    "verified": true,
+    "packed": false,
+    "verified": false,
   },
   "AirdropERC1155": {
     "className": "AirdropERC1155",
     "deployedAddresses": {
       "1": "", // mainnet
-      "5": "0xFDb9cc34BfA6a51e96F73f0a828717A6dfA460E8", // goerli
+      "5": "", // goerli
       "137": "", // polygon
-      "80001": "0xFDb9cc34BfA6a51e96F73f0a828717A6dfA460E8", // mumbai
+      "80001": "", // mumbai
       "250": "", // fantom
       "4002": "", // fantom_testnet
       "43114": "", // avax
@@ -448,8 +448,8 @@ var deployContracts = {
     },
     "needPack": true,
     "needVerify": true,
-    "packed": true,
-    "verified": true,
+    "packed": false,
+    "verified": false,
   },
 };
 var needToPacks = [];
